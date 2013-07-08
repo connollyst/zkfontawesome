d7d40345f493786f8eee452cc04915c5f6511089
Sean Connolly <connolly.st@gmail.com>
2013-07-07 20:55:36 -0400

    Added support for scaled icon sizes (large, 2x, 3x, 4x)

--- src/main/java/org/zkoss/fontawesome/Icon.java
+++ src/main/java/org/zkoss/fontawesome/Icon.java
@@ -10,16 +10,36 @@ import org.zkoss.zul.impl.XulElement;
  */
 public class Icon extends XulElement {
 
+    private static final String DEFAULT_TYPE = "icon-thumbs-up";
+	private static final String DEFAULT_SIZE = "normal";
 	private String type = "";
+	private String size = DEFAULT_SIZE;
 
 	public String getType() {
 		return type;
 	}
 
 	public void setType(String type) {
+        if (type == null) {
+            type = DEFAULT_TYPE;
+        }
 		if (!this.type.equals(type)) {
 			this.type = type;
 			smartUpdate("type", this.type);
+		}
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		if (size == null) {
+			size = DEFAULT_SIZE;
+		}
+		if (!this.size.equals(size)) {
+			this.size = size;
+			smartUpdate("size", this.size);
 		}
 	}
 
@@ -27,6 +47,7 @@ public class Icon extends XulElement {
 			throws IOException {
 		super.renderProperties(renderer);
 		render(renderer, "type", type);
+		render(renderer, "size", size);
 	}
 
 }
--- src/main/resources/web/js/zul/fontawesome/Icon.js
+++ src/main/resources/web/js/zul/fontawesome/Icon.js
@@ -1,27 +1,59 @@
 zul.fontawesome.Icon = zk.$extends(zk.Widget, {
 
-    _type: '',
+    _defaultType: 'icon-thumbs-up',
+    _defaultSize: 'normal',
 
+    _type: 'icon-thumbs-up',
+    _size: 'normal',
 
     getType: function () {
         return this._type;
     },
 
     setType: function (type) {
+        if (!type) {
+            type = this._defaultType;
+        }
         if (this._type != type) {
             this._type = type;
-            if (this.desktop) this.$n().className = zUtl.encodeXML(type);
+            if (this.desktop) this.$n().className = this.domClass_();
+        }
+    },
+
+    getSize: function () {
+        return this._size;
+    },
+
+    setSize: function (size) {
+        if (!size) {
+            size = this._defaultSize;
+        }
+        if (this._size != size) {
+            this._size = size;
+            if (this.desktop) this.$n().className = this.domClass_();
         }
     },
 
     domClass_: function (no) {
         var classes = this.$supers("domClass_", no) || '';
         if (classes) {
-            classes += ', ' + this._type;
-        } else {
-            classes = this._type;
+            classes += ' ';
         }
+        classes += this.fontawesomeClasses_();
         return classes;
+    },
+
+    fontawesomeClasses_: function () {
+        return   this._type + ' ' + this._fontawesomeSize();
+    },
+
+    _fontawesomeSize: function () {
+        switch (this._size) {
+            case 'normal':
+                return '';
+            default:
+                return 'icon-' + this._size;
+        }
     }
 
 }, {
